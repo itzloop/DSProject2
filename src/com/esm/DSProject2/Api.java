0171960117221e1bc562409f9cf2f67e5bbd7328
Amir Esmaeili <amiresm17@gmail.com>
2020-01-02 00:15:31 +0330

    Terminal ui completed

--- src/com/esm/DSProject2/Api.java
+++ src/com/esm/DSProject2/Api.java
@@ -3,26 +3,32 @@ import java.util.Scanner;
 
 public class Api {
     public static void main(String[] args) {
+        System.out.println("Welcome. 2nd project of Data Structures");
+        System.out.println("Enter any command");
         Scanner sc = new Scanner(System.in);
         String command = sc.nextLine();
         DataStructure ds = new DataStructure();
         while (true) {
-            if (command.contains("Show‬‬ ‫‪sup‬‬ ‫‪of‬‬ ‫‪2‬‬ ‫‪sets‬‬")) {
-                ds.sup(command);
-            } else if (command.contains("Show‬‬ ‫‪conf‬‬ ‫‪of‬‬ ‫‪2‬‬ ‫‪sets‬‬")) {
-                ds.conf(command);
-            } else if (command.contains("‫‪Show‬‬ ‫‪all‬‬ ‫‪sets‬‬ ‫‪with‬‬ ‫‪sups‬‬ ‫‪more‬‬")) {
-                // TODO : 3rd function call
+            if (command.contains("Show sup of 2 sets")) {
+                float sup = ds.sup(command);
+                System.out.println(sup * 100 + "%");
+            } else if (command.contains("Show conf of 2 sets")) {
+                float conf = ds.conf(command);
+                System.out.println(conf*100 + "%");
+            } else if (command.contains("Show all sets with sups more than")) {
+                ds.apriori(command);
             } else if (command.contains("file")) {
                 System.out.print("Enter File Path: ");
                 ds.fromFile(sc.nextLine());
             } else if (command.contains("insert")) {
-                ds.insert(command);
+                System.out.println("Enter your products separated by (;) :");
+                ds.insert(sc.nextLine());
             } else if (command.equals("exit")) {
                 break;
             } else {
                 System.out.println("Invalid Command");
             }
+            command = sc.nextLine();
         }
     }
 }
